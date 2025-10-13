6f55dac4902649d05a8136c3178dccf35baef177
mehdinil <mehidhasssz3@gmail.com>
2025-10-13 16:21:44 -0400

    build: set base '/Bnvis/' and outDir '../docs'

--- benevis-pwa/vite.config.ts
+++ benevis-pwa/vite.config.ts
@@ -1,20 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'node:path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/benevis-pwa/',
   plugins: [react()],
-  server: {
-    port: 5173
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  build: {
-    outDir: 'docs',
-    sourcemap: false
-  }
-});
+  base: '/Bnvis/',
+  build: { outDir: '../docs' }
+})
